e9d5de615af20701170364c9f31b9455d94231f7
BoyanVD <46243260+BoyanVD@users.noreply.github.com>
2020-03-04 10:35:42 +0200

    Commented code removed

--- UndoableList/src/undoableList/Undoable.java
+++ UndoableList/src/undoableList/Undoable.java
@@ -5,26 +5,3 @@ import java.util.List;
 interface Undoable<T> {
     public void undo(List<T> list) throws UndoableListException;
 }
-
-//Must create the following delete actions :
-//boolean remove(Object var1);Done
-
-//boolean addAll(Collection<? extends E> var1); Done
-
-//boolean removeAll(Collection<?> var1); Done
-
-//default void replaceAll
-
-
-//default void sort(Comparator<? super E> c)
-
-
-//void clear(); Done
-
-//    boolean add(E var1); Done
-
-//    boolean remove(Object var1); Done
-
-//    void add(int var1, E var2); Done
-
-//    remove by index Done
